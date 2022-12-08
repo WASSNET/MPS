e11086c27de8786755f01e49e8debc8f0a744157
Dax <darronmgordon@gmail.com>
2022-12-08 15:26:45 +0200

    contactPageStart

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,10 +9,24 @@ return(
 </div>
 
 <div className="contactInfo">
-
+<h3>CALL</h3>
+<p>Call: (000) 000-0000</p>
+<p>Call: (000) 000-0000</p>
+<p>Call: (000) 000-0000</p>
+<br></br>
+<h3>Email</h3>
+<p>Email: Admin@MPSZA.co.za</p>
+<p>Email: Sales@MPSZA.co.za</p>
+<p>Email: Technical@MPSZA.co.za</p>
 </div>
 
 <div className="contactAddress">
+
+</div>
+
+<h3>Or Visit us</h3>
+
+<div className="directionMap">
 
 </div>
 
